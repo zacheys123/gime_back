685bdf1a2361d228ba95a7b593402c4ba9ff9008
zacharia <zacheyshop@gmail.com>
2024-01-17 06:01:58 +0300

    all new back end chatapp

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import dotenv from 'dotenv'
+import mongoose from  "mongoose"
+import morgan from "morgan"
+
+dotenv.config()
+
+const source=process.env.MONGOURI
+const app = express()
+
+
+app.use(function (res,req,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,PATCH,DELETE,PUT');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials',true);
+    next()
+})
+app.use(morgan('dev'));
+app.use(express.json({limit:'100mb',extended:true}))
+app.use(express.urlencoded({limit:'100mb',extended:true}))
+const PORT=process.env.PORT || 3500
+
+mongoose.set('strictQuery',true)
+
+mongoose.connect(source).then(()=>{
+    app.listen(()=>console.log('listening at port 5000'))
+}).catch((err)=>console.log(err.message))
+
+const db=mongoose.connection
+db.on('error',(err)=>console.log('err,message'))
+db.once('open',()=>{console.log( 'connection to mongodb established')})
