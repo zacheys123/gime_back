6993119a13ef01f9f8a7a53d61663b01f5b2e868
zacharia <zacheyshop@gmail.com>
2024-01-24 17:41:57 +0300

    created controllers

---
+++ controllers/authController.js
@@ -0,0 +1,6 @@
+export const register=async(req,res)=>{
+    console.log('register')
+}
+export const login=async(req,res)=>{
+    console.log('login')
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import dotenv from 'dotenv'
 import mongoose from  "mongoose"
 import morgan from "morgan"
-
+import authRoutes from './routes/authRoutes.js'
 dotenv.config()
 
 const source=process.env.MONGOURI
@@ -21,6 +21,8 @@ app.use(morgan('dev'));
 app.use(express.json({limit:'100mb',extended:true}))
 app.use(express.urlencoded({limit:'100mb',extended:true}))
 const PORT=process.env.PORT || 3500
+
+app.use('/auth',authRoutes)
 
 mongoose.set('strictQuery',true)
 
